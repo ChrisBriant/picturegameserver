70215d52798487f41bcad4b4ef43e1ff40f3b3e3
Chris Briant <cbriant@btinternet.com>
2021-01-28 20:24:04 +0000

    Working through getting app to work with Redis so I can then start on the game

--- server2b.py
+++ server2b.py
@@ -61,7 +61,7 @@ class BroadcastServerProtocol(WebSocketServerProtocol):
                 #Broadcast client list now a name is set
                 self.factory.send_client_list()
                 #Broadcast room list
-                #self.factory.send_room_list()
+                self.factory.send_room_list()
             elif received_data['type'] == 'enter_room':
                 self.factory.enter_room(received_data['client_id'],received_data['name'])
             elif received_data['type'] == 'exit_room':
@@ -90,7 +90,7 @@ class BroadcastServerFactory(WebSocketServerFactory):
     def __init__(self, url):
         WebSocketServerFactory.__init__(self, url)
         self.clients = {}
-        self.rooms = {}
+        self.rooms = []
         self.timers = {}
 
     #Stores an object in Redis
@@ -104,6 +104,9 @@ class BroadcastServerFactory(WebSocketServerFactory):
         print('This is from redis',dict_obj)
         return dict_obj
 
+    def remove_from_store(self,id):
+        R.delete(id)
+
     def get_clients_from_store(self):
         cli_list = []
         for cli in self.clients.keys():
@@ -111,6 +114,14 @@ class BroadcastServerFactory(WebSocketServerFactory):
             dict_obj = ast.literal_eval(obj.decode('utf-8'))
             cli_list.append(dict_obj)
         return cli_list
+
+    def get_rooms_from_store(self):
+        room_list = []
+        for room in self.rooms:
+            obj = R.get(room)
+            dict_obj = ast.literal_eval(obj.decode('utf-8'))
+            room_list.append(dict_obj)
+        return room_list
 
     def register(self, client):
         registered = [self.clients[i] for i in list(self.clients.keys())]
@@ -134,7 +145,6 @@ class BroadcastServerFactory(WebSocketServerFactory):
 
     def send_client_list(self):
         #Get the clients and create
-        print('Coo ee')
         #clients_data = [{'id':cli, 'name':self.clients[cli]['name']} for cli in clients_list]
         clients_data = self.get_clients_from_store()
         payload = {
@@ -151,27 +161,28 @@ class BroadcastServerFactory(WebSocketServerFactory):
 
     #The connection is closed tidy up
     def unregister(self, client):
-        client_id = None
+        #client_id = None
         all_clients = list(self.clients)
         for cli in all_clients:
-            if client == self.clients[cli]['client']:
+            if client == self.clients[cli]:
                 client_id = cli
                 #Notify room
-                self.exit_room(client_id,self.clients[client_id]['room'])
+                print('Here is the client ID ',client_id)
+                self.exit_room(client_id,self.get_from_store(client_id)['room'])
                 del self.clients[cli]
+                self.remove_from_store(cli)
 
         #For now (for testing purposes destroy all the rooms)
         #Timer will be used to closed rooms when implemented
         #!!!!!!!!!!!!!!!! IMPORTANT BELOW MUST BE REMOVED IN PRODUCTION FOR IT TO WORK !!!!!!!!!!!!!!!#
-        self.rooms = {}
+        self.rooms = []
 
 
     def broadcast(self, msg):
         print("broadcasting message '{}' to {} clients ...".format(msg, len(self.clients)))
         cids = self.clients.keys()
         for cid in cids:
-            print('here is a client', self.clients[cid])
-            self.clients[cid]['client'].sendMessage(msg.encode('utf-8'))
+            self.clients[cid].sendMessage(msg.encode('utf-8'))
 
     def send_client(self,client_id,data):
         print('sending to ',client_id)
@@ -184,12 +195,14 @@ class BroadcastServerFactory(WebSocketServerFactory):
         self.store_object(client_id,client_obj)
 
     def create_room(self,client_id,room):
-        rooms = self.rooms.keys()
-        if room not in rooms:
-            self.rooms[room] = dict()
-            self.rooms[room]['owner'] = client_id
-            self.rooms[room]['name'] = room
-            self.rooms[room]['members'] = []
+        if room not in self.rooms:
+            self.rooms.append(room)
+            room_obj = {
+                'owner' : client_id,
+                'name' : room,
+                'members' : []
+            }
+            self.store_object(room,room_obj)
             self.timers[room] = dict()
             self.timers[room]['timer'] = TimerReset(ROOM_TIMEOUT_VALUE,self.close_room,args=[room])
             self.timers[room]['timer'].start()
@@ -200,69 +213,76 @@ class BroadcastServerFactory(WebSocketServerFactory):
                 'type' : 'room_failure',
                 'reason': 'Room already exists'
             }
-            self.clients[client_id]['client'].sendMessage(json.dumps(send_payload).encode('utf-8'))
+            self.clients[client_id].sendMessage(json.dumps(send_payload).encode('utf-8'))
 
     def send_room_list(self):
-            room_list = [ self.rooms[k] for k in self.rooms.keys()]
             send_payload = {
                 'type' : 'room_list',
-                'rooms': json.dumps(room_list)
+                'rooms': json.dumps(self.get_rooms_from_store())
             }
             cids = self.clients.keys()
             for cid in cids:
                 try:
-                    self.clients[cid]['client'].sendMessage(json.dumps(send_payload).encode('utf-8'))
+                    self.clients[cid].sendMessage(json.dumps(send_payload).encode('utf-8'))
                 except Exception as e:
                     print(e)
 
     def enter_room(self,client_id,room_name):
-        room = self.rooms[room_name]
-        self.clients[client_id]['room'] = room_name
+        room = self.get_from_store(room_name)
+        client = self.get_from_store(client_id)
+        client['room'] = room_name
+        self.store_object(client_id,client)
+
         if client_id not in room['members']:
             room['members'].append(client_id)
             send_payload = {
                 'type' : 'room_entrance',
-                'client': { 'id':client_id, 'name':self.clients[client_id]['name']},
+                'client': { 'id':client_id, 'name':client['name']},
                 'name' : room_name,
-                'members' : [ { 'id' : memb ,'name':self.clients[memb]['name'] } for memb in room['members']]
+                'members' : [ { 'id' : memb ,'name':self.get_from_store(memb)['name'] } for memb in room['members']]
             }
             self.send_room(room,send_payload)
+            self.store_object(room_name,room)
 
     #Send data to a room
     def send_room(self,room,payload):
         for cid in room['members']:
             try:
-                self.clients[cid]['client'].sendMessage(json.dumps(payload).encode('utf-8'))
+                self.clients[cid].sendMessage(json.dumps(payload).encode('utf-8'))
             except Exception as e:
                 print(e)
 
     def exit_room(self,client_id,room_name):
-        room = self.rooms[room_name]
-        self.clients[client_id]['room'] = None
+        print('Let me out of here!')
+        room = self.get_from_store(room_name)
+        client = self.get_from_store(client_id)
         if client_id in room['members']:
             send_payload = {
                 'type' : 'room_exit',
-                'client': { 'id':client_id, 'name':self.clients[client_id]['name']},
+                'client': { 'id':client_id, 'name':client['name']},
                 'name' : room_name,
-                'members' : [ { 'id' : memb ,'name':self.clients[memb]['name'] } for memb in room['members']]
+                'members' : [ { 'id' : memb ,'name':self.get_from_store(memb)['name'] } for memb in room['members']]
             }
             print('send_payload', send_payload)
             self.send_room(room,send_payload)
             #Now remove the member
             room['members'].remove(client_id)
+            self.store_object(room_name,room)
+        client['room'] = None
+        self.store_object(client_id,client)
 
     def close_room(self,room_name):
         print('Closing Room: ', room_name)
-        if room_name in self.rooms.keys():
+        if room_name in self.rooms:
             #Message the users in the room to make it exit
-            room = self.rooms[room_name]
+            room = self.get_from_store(room_name)
             print('Room members are ', room['members'])
             for client_id in room['members']:
                 send_payload = {
                     'type' : 'destroy_room',
                 }
                 self.send_room(room,send_payload)
-            del self.rooms[room_name]
+            self.remove_from_store(room_name)
             del self.timers[room_name]
             self.send_room_list()
 
