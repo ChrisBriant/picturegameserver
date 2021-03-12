d0483b6da306e0eb2547f025feb655befe6a55d3
Chris Briant <cbriant@btinternet.com>
2021-03-12 19:06:02 +0000

    Set up to limit five players

--- tests.py
+++ tests.py
@@ -123,6 +123,73 @@ def test_3_player():
     driver.switch_to_window(driver.window_handles[1])
     driver.find_element_by_id("start-game").click()
 
+
+def test_5_player():
+    #Open four sessions
+    for i in range(0,4):
+        driver.execute_script('window.open("");')
+        driver.get('http://localhost:5000/')
+        letters = string.ascii_letters
+        name = ''.join(random.choice(letters) for i in range(6))
+        driver.find_element_by_id("name").send_keys(name)
+        driver.find_element_by_id("sendname").click()
+        driver.switch_to_window(driver.window_handles[-1])
+    driver.get('http://localhost:5000/')
+    letters = string.ascii_letters
+    name = ''.join(random.choice(letters) for i in range(6))
+    driver.find_element_by_id("name").send_keys(name)
+    driver.find_element_by_id("sendname").click()
+
+    #Create two rooms
+    driver.switch_to_window(driver.window_handles[0])
+    driver.find_element_by_id("room-name").send_keys('Room A')
+    driver.find_element_by_id("sendroom").click()
+    #First two windows open room a
+    driver.switch_to_window(driver.window_handles[0])
+    driver.find_element_by_id("Room A").click()
+    driver.switch_to_window(driver.window_handles[1])
+    driver.find_element_by_id("Room A").click()
+    driver.switch_to_window(driver.window_handles[2])
+    driver.find_element_by_id("Room A").click()
+    driver.switch_to_window(driver.window_handles[3])
+    driver.find_element_by_id("Room A").click()
+    driver.switch_to_window(driver.window_handles[4])
+    driver.find_element_by_id("Room A").click()
+    #Start the game
+    driver.switch_to_window(driver.window_handles[1])
+    #driver.find_element_by_id("start-game").click()
+
+def test_5_player_giveup():
+    test_5_player()
+    for i in range(0,4):
+        for j in range(0,5):
+            try:
+                driver.switch_to_window(driver.window_handles[j])
+                driver.find_element_by_id("giveup").click()
+            except Exception as e:
+                print(e)
+        for k in range(0,5):
+            try:
+                driver.switch_to_window(driver.window_handles[k])
+                driver.find_element_by_id("continue").click()
+            except Exception as e:
+                print(e)
+
+def test_limit_5():
+    test_5_player()
+    #Open 6th window shouldn't be able to join room
+    driver.execute_script('window.open("");')
+    driver.switch_to_window(driver.window_handles[2])
+    driver.get('http://localhost:5000/')
+    letters = string.ascii_letters
+    name = ''.join(random.choice(letters) for i in range(6))
+    driver.find_element_by_id("name").send_keys(name)
+    driver.find_element_by_id("sendname").click()
+
+
+
+
+
 def test_endgame():
     #two player
     test_startgame()
@@ -384,6 +451,12 @@ if __name__ == '__main__':
         test_endgame()
     elif sys.argv[1] == 'test_3_player':
         test_3_player()
+    elif sys.argv[1] == 'test_5_player':
+        test_5_player()
+    elif sys.argv[1] == 'test_5_player_giveup':
+        test_5_player_giveup()
+    elif sys.argv[1] == 'test_limit_5':
+        test_limit_5()
     elif sys.argv[1] == 'test_room_timer':
         test_room_timer()
     # elif sys.argv[1] == 'test_setuproom':
